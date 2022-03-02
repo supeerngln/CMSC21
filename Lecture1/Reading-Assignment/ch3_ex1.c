b2e2491216bcb8e2ac347ae6b9d69bdb2a6ce1f8
Engelene Anuran <89845302+supeerngln@users.noreply.github.com>
2022-03-03 02:02:17 +0800

    Delete ch3_ex1.c

--- Lecture1/Reading-Assignment/ch3_ex1.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h> /*including the <stdio.h> to call the C's standard library to make the program perform input and output operations*/
-int main (void){ /*the main function is automatically called when the code is running*/
-printf("To C, or not to C: that is the question.\n"); /*the programmer wrote the function to perform a specific task and will now it will print a statement*/
-return 0; /*the return 0 is required by the main function for the program to perform accordingly*/
-}
