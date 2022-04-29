96251fc8bb3e6e90ebc88b6e23e8c2e7e753db45
unknown <emanuran@up.edu.ph>
2022-04-29 18:30:30 +0800

    update

---
+++ Lecture 6/lecture6.7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Size of char data type: %u\n", sizeof(char));
+    printf("Size of int data type: %u\n", sizeof(int));
+    printf("Size of float data type: %u\n", sizeof(float));
+    printf("Size of double date type: %u\n", sizeof(double));
+
+    return 0;
+}
