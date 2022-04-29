5e6e0d70dda09a5bea359bd745001957796135e5
unknown <emanuran@up.edu.ph>
2022-04-29 17:07:28 +0800

    update

--- Lecture1/Reading-Assignment/ch3_ex1.c
+++ Lecture1/Reading-Assignment/ch3_ex1.c
@@ -1,15 +1,5 @@
-#include <stdio.h> // the program in order to run should include this to access the C's standard library
-int main(void){  //including the main function since it will be automatically called when the program is executed
-int i, j;  // declaring some variables in integer type
-float x, y;  // declaring float type variables x and y
-
-// the variables i, j, x and y has values equivalent to it through assignments
-i = 10;
-j = 20;
-x = 43.2892f;
-y = 5527.0f;
-printf("i = %d, j = %d, x = %f, y = %f \n", i, j, x, y); //the %d is telling the program that it will print an integer
-                                                         // the %f is telling the program that it will print a float number
-                                                        // this means that the programmer print some messages to indicate the variables given and their values
-return 0;  // return 0 is for the program's termination, it means that it is already done
+#include <stdio.h> /*including the <stdio.h> to call the C's standard library to make the program perform input and output operations*/
+int main (void){ /*the main function is automatically called when the code is running*/
+printf("To C, or not to C: that is the question.\n"); /*the programmer wrote the function to perform a specific task and will now it will print a statement*/
+return 0; /*the return 0 is required by the main function for the program to perform accordingly*/
 }
